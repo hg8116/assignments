d44707caa08a2d0ee78c8969dfa6fc84fe3dc41b
hg8116 <2011.harshgupta@gmail.com>
2023-12-10 21:01:49 +0530

    bug fix

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -1,8 +1,14 @@
+/*
+ * Write 3 different functions that return promises that resolve after 1, 2, and 3 seconds respectively.
+ * Write a function that uses the 3 functions to wait for all 3 promises to resolve using Promise.all,
+ * Print how long it took for all 3 promises to resolve.
+ */
+
 function waitOneSecond() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("waitOneSecond Completed")
-    }, 1000)
+    }, 200)
   })
 }
 
@@ -10,7 +16,7 @@ function waitTwoSecond() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("waitTwoSecond Completed")
-    }, 2000)
+    }, 200)
   })
 }
 
@@ -18,19 +24,37 @@ function waitThreeSecond() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("waitThreeSecond Completed")
-    }, 3000)
+    }, 200)
   })
 }
 
 function calculateTime() {
-  const startTime = Date.now()
-  Promise.all([waitOneSecond(), waitTwoSecond(), waitThreeSecond()]).then(
-    (res) => {
-      console.log(res)
-      console.log("All 3 promises resolved in", Date.now() - startTime, "ms")
-    }
-  )
+  let start = Date.now()
+  return Promise.all([
+    waitOneSecond(),
+    waitTwoSecond(),
+    waitThreeSecond(),
+  ]).then((res) => {
+    console.log(
+      res[0],
+      res[1],
+      res[2],
+      "All 3 promises resolved in",
+      Date.now() - start,
+      "ms"
+    )
+    return [
+      res[0],
+      res[1],
+      res[2],
+      "All 3 promises resolved in",
+      Date.now() - start,
+      "ms",
+    ]
+  })
 }
+
+calculateTime()
 
 module.exports = {
   calculateTime,
